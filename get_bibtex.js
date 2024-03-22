0ce7f3c7cdb34d53eb7f8d41c74643812a673691
Bhuvnesh Brawar <90771976+bbrawar@users.noreply.github.com>
2024-03-22 15:31:15 +0530

    Create get_bibtex.js

---
+++ get_bibtex.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('getBibtexBtn').addEventListener('click', function() {
+        var doi = document.getElementById('doi').value;
+        var apiUrl = "https://api.crossref.org/works/" + doi;
+
+        $.get(apiUrl, function(data) {
+            if (data.message && data.message.title && data.message.author && data.message['published-print']) {
+                var title = data.message.title[0];
+                var authors = data.message.author.map(function(author) {
+                    return author.family + ', ' + author.given;
+                }).join(' and ');
+
+                var publishedDate = data.message['published-print']['date-parts'][0];
+                var year = publishedDate[0];
+                var month = publishedDate[1];
+
+                var volume = data.message.volume;
+                var issue = data.message.issue;
+                var pages = data.message.page;
+                var journal = data.message['container-title'][0];
+                var publisher = data.message.publisher;
+
+                var bibtex = '@article{' + doi + ',\n' +
+                             '  doi = {' + doi + '},\n' +
+                             '  url = {' + data.message.URL + '},\n' +
+                             '  year = {' + year + '},\n' +
+                             '  month = {' + month + '},\n' +
+                             '  publisher = {' + publisher + '},\n' +
+                             '  volume = {' + volume + '},\n' +
+                             '  number = {' + issue + '},\n' +
+                             '  pages = {' + pages + '},\n' +
+                             '  author = {' + authors + '},\n' +
+                             '  title = {' + title + '},\n' +
+                             '  journal = {' + journal + '}\n' +
+                             '}';
+                
+                document.getElementById('bibtex').innerText = bibtex;
+            } else {
+                document.getElementById('bibtex').innerText = "BibTeX data not found for the provided DOI.";
+            }
+        });
+    });
+});
